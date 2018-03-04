kodi-game/kodi-game-scripting

commit 2b9073f583975364abbb46f16e63a8377028afae

addon.xml: Add language tags

templates/addon/game.libretro.{{ game.name }}/addon.xml.in.j2
@@ -25,7 +25,7 @@
 		{% if system_info.version %}
 			{% set summary = summary + ' ' + system_info.version %}
 		{% endif %}
-		<summary lang="en">{{ summary | e }}</summary>
+		<summary lang="en_GB">{{ summary | e }}</summary>
 		{% if system_info.extensions %}
 			{% set extensions = 'Supported files: ' + system_info.extensions | surround('.', append=False) | join(', ') %}
 		{% endif %}
@@ -39,7 +39,7 @@
 			{% set description = description | regex_replace('License: .*', '') %}
 		{% endif %}
 		{% set description = description | regex_replace('^(\s*\\n){2,}', '\n', multiline=True) | trim %}
-		<description>{{ description | e }}</description>
+		<description lang="en_GB">{{ description | e }}</description>
 		{% if xml.addon.extension[1].disclaimer %}
 		<disclaimer>{{ xml.addon.extension[1].disclaimer | e }}</disclaimer>
 		{% endif %}

commit e83cb69328403c39d4ef773055dbda8b94f4986f

template_processor: Fix script after adding language attributes
When parsing the existing addon.xml.in files, xmljson created a
shortened output if the element didn't contain any attributes.

This behavior broke the script with 2b9073f where attributes were added
and the element content has to be accessed with '.content' now.

Disable the behavior in xmljson and always for the creation of
'content' fields so that we always access the content the same way
(no matter if there are attributes or not).

kodi_game_scripting/template_processor.py
@@ -104,8 +104,15 @@ class TemplateProcessor:
                                          xml_content)
                     xml_data = {}
                     try:
+                        # Like Yahoo converter, but don't omit 'content' if
+                        # there are no attributes.
+                        converter = xmljson.XMLData(
+                            xml_fromstring=False,
+                            simple_text=False,
+                            text_content="content"
+                        )
                         root = xml.etree.ElementTree.fromstring(xml_content)
-                        xml_data = xmljson.Yahoo().data(root)
+                        xml_data = converter.data(root)
 
                         # Parsed XML Data will contain OrderedDict() as empty
                         # value which converts to 'OrderedDict()' instead of ''

templates/addon/game.libretro.{{ game.name }}/addon.xml.in.j2
@@ -15,7 +15,7 @@
 	</requires>
 	<extension point="kodi.gameclient"
 			library_@PLATFORM@="@LIBRARY_FILENAME@">
-		<platforms>{{ xml.addon.extension[0].platforms | default('') | e }}</platforms>
+		<platforms>{{ xml.addon.extension[0].platforms.content | default('') | e }}</platforms>
 		<extensions>{{ system_info.extensions | join('|') | e }}</extensions>
 		<supports_vfs>{{ 'false' if system_info.need_fullpath | default('true') else 'true' }}</supports_vfs>
 		<supports_standalone>{{ system_info.supports_no_game | default('false') | lower }}</supports_standalone>
@@ -29,21 +29,21 @@
 		{% if system_info.extensions %}
 			{% set extensions = 'Supported files: ' + system_info.extensions | surround('.', append=False) | join(', ') %}
 		{% endif %}
-		{% set description = xml.addon.extension[1].description | default('') %}
-		{% if ("Supported files: " in xml.addon.extension[1].description) %}
-			{% set description = xml.addon.extension[1].description | regex_replace('Supported files: (?:\.\w+)(?:,\s*\.\w+)*', extensions | default('')) %}
+		{% set description = xml.addon.extension[1].description.content | default('') %}
+		{% if ("Supported files: " in xml.addon.extension[1].description.content) %}
+			{% set description = xml.addon.extension[1].description.content | regex_replace('Supported files: (?:\.\w+)(?:,\s*\.\w+)*', extensions | default('')) %}
 		{% elif extensions %}
-			{% set description = xml.addon.extension[1].description | default('') + '\n\n' + extensions %}
+			{% set description = xml.addon.extension[1].description.content | default('') + '\n\n' + extensions %}
 		{% endif %}
-		{% if ("License: " in xml.addon.extension[1].description) %}
+		{% if ("License: " in xml.addon.extension[1].description.content) %}
 			{% set description = description | regex_replace('License: .*', '') %}
 		{% endif %}
 		{% set description = description | regex_replace('^(\s*\\n){2,}', '\n', multiline=True) | trim %}
 		<description lang="en_GB">{{ description | e }}</description>
 		{% if xml.addon.extension[1].disclaimer %}
-		<disclaimer>{{ xml.addon.extension[1].disclaimer | e }}</disclaimer>
+		<disclaimer>{{ xml.addon.extension[1].disclaimer.content | e }}</disclaimer>
 		{% endif %}
-		<license>{{ libretro_info.license | default(xml.addon.extension[1].license) | default('') | e }}</license>
+		<license>{{ libretro_info.license | default(xml.addon.extension[1].license.content) | default('') | e }}</license>
 		<platform>@PLATFORM@</platform>
 		{% if assets %}
 		<assets>
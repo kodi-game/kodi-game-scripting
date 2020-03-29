73d6f1072264f4bfb45a76fffea296b0e5ce131f
Garrett Brown <themagnificentmrb@gmail.com>
2020-03-29 12:09:46 -0700

    Revert "config: Disable lutro due to recently broken build"
    This reverts commit bb8a0d333d01030fa1d5576553de723e0315bd97.

--- kodi_game_scripting/config.py
+++ kodi_game_scripting/config.py
@@ -62,7 +62,7 @@ ADDONS = {
     'gw':                        ('gw-libretro',                'Makefile',          '.',                 'jni', {}),
     'handy':                     ('libretro-handy',             'Makefile',          '.',                 'jni', {}),
     'hatari':                    ('hatari',                     'Makefile.libretro', '.',                 'jni', {}),
-    #'lutro':                     ('libretro-lutro',             'Makefile',          '.',                 'jni', {}),  # Build broke due to PR merged on 2020-01-04
+    'lutro':                     ('libretro-lutro',             'Makefile',          '.',                 'jni', {}),
     #'mame':                      ('mame',                       'Makefile.libretro', '.',                 'jni', {'cmake_options': 'PTR64=1'}),  # Huge and longrunning
     'mame2000':                  ('mame2000-libretro',          'Makefile',          '.',                 'jni', {}),
     'mame2003':                  ('mame2003-libretro',          'Makefile',          '.',                 'jni', {}),

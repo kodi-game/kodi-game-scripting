aced55f4eab8869c14113a311410aac4eba9a93f
Garrett Brown <themagnificentmrb@gmail.com>
2020-09-01 12:30:55 -0700

    config: Add 3D Engine

--- kodi_game_scripting/config.py
+++ kodi_game_scripting/config.py
@@ -27,6 +27,7 @@ GITHUB_ADDON_PREFIX = 'game.libretro.'
 # core: {(Libretro repo, Makefile, Directory)}
 ADDONS = {
     '2048':                      ('libretro-2048',              'Makefile.libretro', '.',                 'jni', {}),
+    '3dengine':                  ('libretro-3dengine',          'Makefile',          '.',                 'jni', {}),
     '81':                        ('81-libretro',                'Makefile',          '.',                 'build/jni', {}),
     'beetle-bsnes':              ('beetle-bsnes-libretro',      'Makefile',          '.',                 'jni', {'soname': 'mednafen_snes'}),
     'beetle-gba':                ('beetle-gba-libretro',        'Makefile',          '.',                 'jni', {'soname': 'mednafen_gba'}),

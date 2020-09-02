9869383d9d626518a73d2c1865e22e5209a46e86
Garrett Brown <themagnificentmrb@gmail.com>
2020-09-02 15:42:45 -0700

    config: Fix Yabause JNI location
    Build results:
    
    https://jenkins.kodi.tv/blue/organizations/jenkins/kodi-game%2Fgame.libretro.yabause/detail/master/52/pipeline

--- kodi_game_scripting/config.py
+++ kodi_game_scripting/config.py
@@ -139,5 +139,5 @@ ADDONS = {
     #'vice':                      ('vice-libretro',              'Makefile.libretro', '.',                 'jni', {'soname': 'vice_x64'}),  # Settings broken for option API version 0
     'virtualjaguar':             ('virtualjaguar-libretro',     'Makefile',          '.',                 'jni', {}),
     'xmil':                      ('xmil-libretro',              'Makefile.libretro', 'libretro',          'libretro/jni', {'soname': 'x1'}),
-    'yabause':                   ('yabause',                    'Makefile',          'yabause/src/libretro',          'libretro/jni', {}),
+    'yabause':                   ('yabause',                    'Makefile',          'yabause/src/libretro', 'yabause/src/libretro/jni', {}),
 }

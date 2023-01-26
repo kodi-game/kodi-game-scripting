3056877139ad598ee941ead2488d64c36052b408
Garrett Brown <themagnificentmrb@gmail.com>
2023-01-26 14:55:32 -0800

    Revert "[temp] Add process.sh"
    This reverts commit f8a418baefb31dd4b11d721505d645c607f36bc7.

--- process.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-./process_game_addons.py \
-    --game-addons-dir=game-addons \
-    --kodi-source-dir=`pwd`/../kodi \
-    `#--push-branch=master` \
-    --git \
-    --compile \
-    --filter="yabause"
